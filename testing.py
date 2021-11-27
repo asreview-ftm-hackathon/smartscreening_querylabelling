ccb9e0ecf42bb6ef8fb85313b76184fbc6f326f7
Matthew vd Meer <matthew.vdmeer@gmail.com>
2021-11-27 15:02:19 +0100

    updated test strings

--- testing.py
+++ testing.py
@@ -1,12 +1,19 @@
 from main import *
 
 
-test_queries = ['()','()','()']
+test_queries = ['(abstract:zalm AND (abstract:evi OR (NOT type:help)))',
+                '((abstract:evi OR (NOT type:help)) AND abstract:zalm)',
+                '(abstract:zalm pasta AND (abstract:evi OR (NOT type:help me)))',
+                '(abstract:zalm pasta AND (evi OR (NOT type:do not help me)))',
+                '(abstract:zalm pasta)',
+                '(abstract:evi AND type:help)',
+                '(abstract:42222*& OR type:life)'
+                ]
 
 
-def test():
-    output = None
-    success = False
+def test(query):
+    output = query
+    success = True
     return success, output
 
 for query in test_queries:
