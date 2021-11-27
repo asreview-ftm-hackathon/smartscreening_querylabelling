ed8327f8dc51f4f1eab978d2971b818ab889c1e2
Matthew vd Meer <matthew.vdmeer@gmail.com>
2021-11-27 16:48:46 +0100

    function skeleton

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-pass
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+
+def load_data(filepath):
+    pass
+
+def parse_query():
+    pass
+
+def get_vocabulary():
+    pass
+
+def get_vectorizer():
+    pass
+
+def vectorize_data():
+    pass
+
+def select_subset():
+    pass
+
+def save_data():
+    pass
+
+if __name__ == '__main__':
+    data_main = load_data('test_data.csv')
